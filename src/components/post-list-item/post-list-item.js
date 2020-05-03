39f9b0e2fe3ea61b0227c0775df274c04dc1d3ca
Maksim Hvatov <uzhas76064@gmail.com>
2020-05-03 16:37:08 +0500

    Added date to the post-item

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -2,6 +2,27 @@ import React from 'react';
 
 import './post-list-item.css';
 
+let currentDate = new Date();
+
+const dateOptions = {
+    year:'numeric',
+    month:'long',
+    day: 'numeric',
+}
+
+const dateConfig = {
+    year:null,
+    month:null,
+    date:null,
+}
+
+dateConfig.year = currentDate.getFullYear().toString();
+dateConfig.month = currentDate.getMonth().toString();
+dateConfig.date = currentDate.getDate().toString();
+
+let postItemDate = new Date(dateConfig.year, dateConfig.month, dateConfig.date, 0, 0, 0, 0).toLocaleString('ru', dateOptions);
+
+
 const PostListItem =() => {
     return (
         <li className="app-list-item d-flex justify-content-between">
@@ -9,6 +30,7 @@ const PostListItem =() => {
                 Hello world!
             </span>
             <div className="d-flex justify-content-center align-items-center">
+                <span>{postItemDate}</span>
                 <button className="btn-star btn-sm">
                     <i className="fa fa-star"></i>
                 </button>
